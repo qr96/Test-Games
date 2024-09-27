8b15ca79f702081696ad2d5952df3957cab8646b
qr96 <qr96@naver.com>
2024-09-27 22:59:47 +0900

    mod MapManager

--- TestGamesClient/Assets/Scripts/GameC/Managers/MapManagerC.cs
+++ TestGamesClient/Assets/Scripts/GameC/Managers/MapManagerC.cs
@@ -7,6 +7,14 @@ public class MapManagerC : MonoBehaviour
     // TODO : Move to Resources folder.
     public List<GameObject> maps = new List<GameObject>();
 
+    private void Start()
+    {
+        foreach (var map in maps)
+            map.SetActive(false);
+
+        MoveMap(0);
+    }
+
     public void MoveMap(int mapId)
     {
         foreach (var map in maps)
