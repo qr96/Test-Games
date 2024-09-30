ed513806f130d3ca4e57d8e7b393bcccabe4a534
qr96 <qr96@naver.com>
2024-09-30 22:52:58 +0900

    add QuestManager

---
+++ TestGamesClient/Assets/Scripts/GameC/LocalServer/QuestManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LocalServerC
+{
+    public class QuestManager
+    {
+        public void StartQuest(int code)
+        {
+
+        }
+
+        public void FinishQuest(int code)
+        {
+
+        }
+
+        public void GiveupQuest(int code)
+        {
+
+        }
+    }
+}
+
